7249c24323e510ac1de9aa8ce3818abe43b6e178
luisfejaramillor <luisfejaramillor@gmail.com>
2024-01-16 23:33:47 -0500

    Add navigation custom hook from react router library

--- 07-heroes-spa/src/ui/components/NavBar.jsx
+++ 07-heroes-spa/src/ui/components/NavBar.jsx
@@ -1,7 +1,14 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const NavBar = () => {
+    const navigate = useNavigate()
+    const handleLogout = ()=> {
+        navigate('/login', {
+            replace: true
+        })
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -28,13 +35,25 @@ export const NavBar = () => {
                     >
                         DC
                     </NavLink>
+                    <NavLink 
+                        className={({isActive})=> `nav-item nav-link ${isActive ? 'active' : ''}`} 
+                        to="/search"
+                    >
+                        Search
+                    </NavLink>
+                    <NavLink 
+                        className={({isActive})=> `nav-item nav-link ${isActive ? 'active' : ''}`} 
+                        to="/hero"
+                    >
+                        Hero
+                    </NavLink>
                 </div>
             </div>
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-primary' >Luis Jaramillo</span>
-                    <button className='nav-item nav-link btn' >Logout</button>
+                    <button onClick={handleLogout} className='nav-item nav-link btn' >Logout</button>
                 </ul>
             </div>
         </nav>
